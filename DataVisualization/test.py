b223d5c0497caa091e3dd76c85e687fe39b8204d
Đinh Sỹ Hào <neko.fujinomiyaki@gmail.com>
2018-10-18 22:54:59 +0700

    Change import
    Change import as 'draw' to 'vsl' (visuallizer)

--- DataVisualization/test.py
+++ DataVisualization/test.py
@@ -1,12 +1,12 @@
-import DataVisualization.DrawChart as draw
+import DataVisualization.DrawChart as vsl #visualizer
 
-df = draw.fileVisualizer("data/housing.csv")
+df = vsl.fileVisualizer("data/housing.csv")
 
 #Draw barh chart plot with cols[0:3], rows[0:10] and stacked
-#draw.drawBar(df, range(10), range(3), True, True)
+#vsl.drawBar(df, range(10), range(3), True, True)
 
 #Draw line chart plot with cols[0:3] and rows[0:10]
-#draw.drawLine(df, range(10), range(3))
+#vsl.drawLine(df, range(10), range(3))
 
 #Draw histogram chart plot with cols[0] and rows[0:max]
-draw.drawHistogram(df, range(len(df.index)), 0, 50)
+vsl.drawHistogram(df, range(len(df.index)), 0, 50)  
